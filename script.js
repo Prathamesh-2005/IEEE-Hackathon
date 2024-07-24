06b8547d1eab347ca461c4e70b4b22b41544bbe3
shubhamshinde5356 <150072714+Techyshubhs@users.noreply.github.com>
2024-07-24 15:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,41 @@ function openSidebar() {
   const sidebar = document.getElementById("mySidebar");
   sidebar.style.display = "inline";
   sidebar.style.position = "fixed";
-  sidebar.style.left = "0"
-  sidebar.style.top = "0"
+  sidebar.style.left = "0";
+  sidebar.style.top = "0";
   sidebar.style.zIndex = "12";
   sidebar.style.width = "180px"; // Set your desired width
+  
+  // Add event listener to close the sidebar when clicking outside of it
+  document.addEventListener('click', closeSidebarOnClickOutside);
 }
+
 function closeSidebar() {
-  const Sidebar = document.getElementById("mySidebar");
-  Sidebar.style.display = "none";
+  const sidebar = document.getElementById("mySidebar");
+  sidebar.style.display = "none";
+  
+  // Remove event listener after closing the sidebar
+  document.removeEventListener('click', closeSidebarOnClickOutside);
 }
-const sidebar = document.getElementById("aside")
-const menu_button = document.getElementById('menu_bar')
-menu_button.addEventListener('click', openSidebar)
 
+function closeSidebarOnClickOutside(event) {
+  const sidebar = document.getElementById("mySidebar");
+  if (!sidebar.contains(event.target) && event.target.id !== 'menu_bar') {
+    closeSidebar();
+  }
+}
 
-const menu_buttonff = document.getElementById('menu_barOff')
-menu_buttonff.addEventListener('click', closeSidebar)
+const menuButton = document.getElementById('menu_bar');
+menuButton.addEventListener('click', (event) => {
+  event.stopPropagation(); // Prevent the click event from bubbling up to the document
+  openSidebar();
+});
 
+const menuButtonOff = document.getElementById('menu_barOff');
+menuButtonOff.addEventListener('click', (event) => {
+  event.stopPropagation(); // Prevent the click event from bubbling up to the document
+  closeSidebar();
+});
 
 const darkModeButton = document.getElementById('dark-mode-button');
 darkModeButton.addEventListener('click', () => {
